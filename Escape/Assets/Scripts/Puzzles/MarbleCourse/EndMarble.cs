fd0443d0102d8648fb157a651e856bc4abb5805d
SamuPaju <151126865+SamuPaju@users.noreply.github.com>
2024-11-28 09:28:02 +0200

    Marble course end fix

--- Escape/Assets/Scripts/Puzzles/MarbleCourse/EndMarble.cs
+++ Escape/Assets/Scripts/Puzzles/MarbleCourse/EndMarble.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EndMarble : MonoBehaviour
 {
+    [SerializeField] private UnityEvent goal;
+    public UnityEvent Goal => goal;
     [SerializeField] public bool puzzleDone = false;
     private void OnTriggerEnter(Collider other)
     {
-        puzzleDone = true;
+        if (other.gameObject.CompareTag("marble"))
+        {
+            puzzleDone = true;
+            StationActivator.instance.QuitStation(2);
+            other.gameObject.SetActive(false);
+            goal.Invoke();
+        }
     }
 }
