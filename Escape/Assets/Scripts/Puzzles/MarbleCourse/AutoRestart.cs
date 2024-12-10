a6c6e99e4aabb78f5063de9519dfcf99fadb1c2f
SamuPaju <151126865+SamuPaju@users.noreply.github.com>
2024-12-10 12:52:44 +0200

    Commented AutoRestart

--- Escape/Assets/Scripts/Puzzles/MarbleCourse/AutoRestart.cs
+++ Escape/Assets/Scripts/Puzzles/MarbleCourse/AutoRestart.cs
@@ -6,6 +6,10 @@ public class AutoRestart : MonoBehaviour
 {
     [SerializeField] MCSettings marbleCourse;
 
+    /// <summary>
+    /// If marble enters this objects boxcollider it restarts the MarbleCourse
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("marble"))
